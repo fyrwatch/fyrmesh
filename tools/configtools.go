760710eceb1c5021091b661eed801881b16cb21d
Manish Meganathan <757manish@gmail.com>
2021-05-16 15:34:48 +0530

    Update Go licenses in the FyrORCH and tools directories

--- tools/configtools.go
+++ tools/configtools.go
@@ -1,3 +1,14 @@
+/*
+===========================================================================
+Copyright (C) 2020 Manish Meganathan, Mariyam A.Ghani. All Rights Reserved.
+
+This file is part of the FyrMesh library.
+No part of the FyrMesh library can not be copied and/or distributed
+without the express permission of Manish Meganathan and Mariyam A.Ghani
+===========================================================================
+FyrMesh gopkg tools
+===========================================================================
+*/
 package tools
 
 import (
